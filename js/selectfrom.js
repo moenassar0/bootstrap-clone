eb935c79f523da3669fabc5de81b5767c52d385a
Mohamad Nassar <mnassar57@gmail.com>
2022-09-14 04:44:42 +0300

    implementing js selection api connection

--- js/selectfrom.js
+++ js/selectfrom.js
@@ -1,1 +1,23 @@
 var table = document.getElementById("table");
+
+const api_url = "./select-api.php";
+
+async function selectAPI(url) {
+	
+	// Storing response
+	const response = await fetch(url);
+	
+	// Storing data in form of JSON
+	var data = await response.json();
+	console.log(data);
+	show(data);
+}
+
+// Calling that async function
+selectAPI(api_url);
+
+function show(data) {
+	for(let i = 0; i < data.length; i++){
+        console.log(data[i].full_name);
+    }
+}
